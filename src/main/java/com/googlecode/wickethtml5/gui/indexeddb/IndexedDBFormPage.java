d28bf4031b8c4c5b71191001e24ebc04504087fc
tom.hombergs <tom.hombergs@gmail.com>
2012-09-16 09:47:57 +0000

    update to work with current chrome

--- src/main/java/com/googlecode/wickethtml5/gui/indexeddb/IndexedDBFormPage.java
+++ src/main/java/com/googlecode/wickethtml5/gui/indexeddb/IndexedDBFormPage.java
@@ -1,15 +1,13 @@
 package com.googlecode.wickethtml5.gui.indexeddb;
 
-
 import com.googlecode.wickethtml5.gui.BasePage;
 import com.googlecode.wickethtml5.gui.indexeddb.behavior.IndexedDBFormBehavior;
-
 
 public class IndexedDBFormPage extends BasePage {
 
 	public IndexedDBFormPage() {
 		EmployeeForm employeeForm = new EmployeeForm("employeeForm");
-		employeeForm.add(new IndexedDBFormBehavior("employeeDB", "employees", "1", "lastName", "/wickethtml5/indexedDBTable"));
+		employeeForm.add(new IndexedDBFormBehavior("employeeDB", "employees", "1", "lastName", "/indexedDBTable"));
 		add(employeeForm);
 	}
 }
